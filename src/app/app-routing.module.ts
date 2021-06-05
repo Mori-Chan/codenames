7052f1d782e8fd2539fe0cbb093539b6ef09d326
Mori-Chan <59697686+Mori-Chan@users.noreply.github.com>
2021-06-05 18:12:35 +0900

    codenames

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LobbyComponent } from './component/lobby/lobby.component';
+import { GameComponent } from './component/game/game.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: LobbyComponent},
+  {path: 'game/:id', component: GameComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
